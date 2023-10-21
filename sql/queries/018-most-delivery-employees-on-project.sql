c6178bd5743be2103327f6f0702f2e3e9fb982e4
Taylor Crawford <tcrawford540@gmail.com>
2023-10-21 16:58:46 +0100

    [018] Add query for most delivery employees on project.

---
+++ sql/queries/018-most-delivery-employees-on-project.sql
@@ -0,0 +1,7 @@
+-- As a member of the Management team I want to be able to see the project with the 
+	-- most delivery employees currently working on it
+
+SELECT P.projectName, COUNT(DEP.employeeID) AS `Count` FROM projects P
+	INNER JOIN deliveryEmployeesProject DEP ON P.projectID = DEP.projectID
+    GROUP BY (P.projectID)
+    ORDER BY `Count` DESC LIMIT 1;

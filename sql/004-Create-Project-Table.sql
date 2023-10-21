487aa6d1322146054b86de4fbd3dfc9e75028831
Taylor Crawford <tcrawford540@gmail.com>
2023-10-21 10:05:04 +0100

    [004] Add SQL Create Delivery Employees Project Table and Test Data Script

--- sql/004-Create-Project-Table.sql
+++ sql/004-Create-Project-Table.sql
@@ -31,3 +31,37 @@ DELIMITER $$
 DELIMITER ;
 
 CALL insertFakeProjectData();
+
+-- Link Table Between Delivery Employees AND Projects, Used To Link Delivery Employees How Have Work Or Are Working On A Project.
+
+CREATE TABLE deliveryEmployeesProject (
+	employeeID INT,
+    projectID INT,
+    startDate DATE NOT NULL,
+    endDate DATE NULL,
+    FOREIGN KEY(employeeID) REFERENCES deliveryEmployees(employeeID),
+    FOREIGN KEY(projectID) REFERENCES projects(projectID),
+    PRIMARY  KEY(employeeID, projectID)
+);
+
+DROP PROCEDURE IF EXISTS insertFakeDeliveryEmployeeProjectData;
+DELIMITER $$
+		CREATE PROCEDURE insertFakeDeliveryEmployeeProjectData()
+			BEGIN
+				START TRANSACTION;
+            
+					INSERT INTO deliveryEmployeesProject (employeeID, projectID, startDate, endDate) 
+						VALUES
+                        (1, 1, "2020-10-13", NULL),
+                        (1, 2, "2022-12-10", "2023-01-15");
+                    GET DIAGNOSTICS @rows = ROW_COUNT;
+						IF @rows != 2 THEN
+							ROLLBACK;
+							SELECT 'Transaction (insertFakeDeliveryEmployeeProjectData) rolled back due to error: ' + @rows;
+						ELSE
+							COMMIT;
+							SELECT 'Transaction (insertFakeDeliveryEmployeeProjectData) committed successfully';
+					END IF;
+			END $$
+DELIMITER ;
+CALL insertFakeDeliveryEmployeeProjectData();
